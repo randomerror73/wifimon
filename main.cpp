fb81a697be69b485394c48c92df575ce74247fa9
Daniel Fiala <d.fiala@freenet.de>
2016-12-14 21:20:02 +0100

    t commit --amend --reset-author git commit --amend --reset-author playing with a global sempaphore aiming for a single instance app (work in progress)

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 #include <QApplication>
 #include <QSystemSemaphore>
 
+class ReleaseSemaphore
+{
+public:
+    ReleaseSemaphore(QSystemSemaphore & sem):semaphore(sem){}
+    ~ReleaseSemaphore(){semaphore.release();}
+    QSystemSemaphore & semaphore;
+};
+
 int main(int argc, char *argv[])
 {
-    QSystemSemaphore semaphore("0cb49fdf-8058-47be-91ca-72d02e7f715d",1);
-    if (!semaphore.acquire())return 0;
+    //QSystemSemaphore semaphore("0cb49fdf-8058-47be-91ca-72d02e7f715d",1);
+    //ReleaseSemaphore guard(semaphore);
+    //if (!semaphore.acquire())return 0;
     QApplication a(argc, argv);
     WifiMon w;
 
